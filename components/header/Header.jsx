b78fcde4ea617f462f815a52341f352239aaeaed
Al-kodach <alkodach351@gmail.com>
2023-03-12 18:55:55 -0400

    setup-open-close-icon

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -1,16 +1,14 @@
-import { context } from "../../hooks/useContext";
-import { useContext } from "react";
 import bugger from "../../assets/icons/buggerIcon";
 import Nav from './Nav';
 
 export default function Header() {
-const {isMenuOpen} = useContext(context)
+
 
   return (
     <header className='flex flex-col px-2 py-4'>
       <div className='flex justify-between'>
         <h1 className='text-2xl uppercase '>Al-Kodach</h1>
-        {bugger}
+        {bugger()}
       </div>
         <Nav />
     </header>
